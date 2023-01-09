4579171fe34ad05beb55380343398b43ce458aef
Rost <coderost@yandex.ru>
2023-01-09 17:48:47 +0300

    Init commit

---
+++ src/main/java/com/rost/KafkaProjectApplication.java
@@ -0,0 +1,11 @@
+package com.rost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProjectApplication.class, args);
+	}
+}
